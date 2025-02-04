e988719537f7e63401d94244b117862fe31cd92d
Abdullah Belal <152031320+Abdullah-Belall@users.noreply.github.com>
2025-02-04 19:10:29 +0200

    base.ts

--- app/base.ts
+++ app/base.ts
@@ -1,2 +1,2 @@
-export const BaseWebsiteLink = "https://localhost:3000";
+export const BaseWebsiteLink = "https://nile-guides-backend-production.up.railway.app";
 export const BaseImagesLink = "https://res.cloudinary.com/doy0la086/image/upload";
